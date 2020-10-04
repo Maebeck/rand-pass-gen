66e39ad20ee32a2475fcf3f61117af58b4363e8b
Corey Smith <CSSmith1997@gmail.com>
2020-10-03 18:02:40 -0600

    Added  all the questions needed for the user input, added if statements to verify inputs, added code to generate the password

--- script.js
+++ script.js
@@ -1,14 +1,60 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var endPass="";
 
 // Write password to the #password input
-function writePassword() {
+function generatePassword() {
+  var passLength = prompt ("How many characters (8-128) do you want in the password?")
+  if (passLength <8 || passLength > 128 ) {
+    alert("Please enter a number between 8-128")
+    generatePassword()
+  } else {
+    var charUpper = confirm ('Would you like any letters to be capital letters?');
+    var charNumb = confirm ('Would you like any numbers in your password?');
+    var charSpec = confirm ('Would you like any special characters in your password?');
+
+    writePassword();
+
+  }
+  functgion writePassword() {
+    var alphaLower = "abcdefghijgklmnopqrstuvwxyz";
+    var alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var numbs = "0123456789"
+    var chars = "!@#$%^&*()_+:;?<>,./-+=";
+    var codeSpec = alphaLower;
+
+
+    if (charUpper) {
+      codeSpec = codeSpec + alphaUpper
+    } if (charNum) 
+      codeSpec = codeSpec + numbs
+  } if (charSpec) {
+    codeSpec = codeSpec + chars
+  }
+
+  console.log("These are your code options " + codeSpec);
+  console.log (codeSpec.length);
+  console.log (passLength);
+  for (var i = passLength; i > 0; i == 0) {
+    endPass = endPass + codeSpec(Math.floor(Math.random() * codeSpec.length))
+  }
+
+
+
+  }
+  
+  
+  
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+  
+
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
